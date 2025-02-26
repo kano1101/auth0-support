d440c42fd8090ebd1d0f6e9e7f1f22cef1c2af1b
Akira Kano <a.kano1101@gmail.com>
2025-02-26 13:24:59 +0900

    インポートしやすくlib.rsにpub useを追加

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,8 @@ pub mod errors;
 pub mod middlewares;
 pub mod traits;
 pub mod utils;
+
+pub use crate::auth::auth::{callback, login, logout};
+pub use crate::config::config::Config;
+pub use crate::middlewares::before::RequireAuthBeforeMiddleware;
+pub use crate::middlewares::after::after_middleware;
